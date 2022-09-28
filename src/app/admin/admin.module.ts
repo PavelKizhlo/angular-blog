8f33c774086581f27fb6713df66f2008e58b3d05
Pavel Kizhlo <pavelkizh@gmail.com>
2022-09-28 16:42:43 +0300

    feat: implement login form and it's validation

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -6,6 +6,7 @@ import { LoginPageComponent } from './login-page/login-page.component';
 import { EditPageComponent } from './edit-page/edit-page.component';
 import { CreatePageComponent } from './create-page/create-page.component';
 import { DashboardPageComponent } from './dashboard-page/dashboard-page.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -17,6 +18,8 @@ import { DashboardPageComponent } from './dashboard-page/dashboard-page.componen
   ],
   imports: [
     CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
     RouterModule.forChild([
       {
         path: '',
