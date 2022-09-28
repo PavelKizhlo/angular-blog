02b0279f1cd15a882647899093d590451400395a
Pavel Kizhlo <pavelkizh@gmail.com>
2022-09-28 23:06:38 +0300

    feat: add route guard

---
+++ src/app/admin/shared/services/auth.guard.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import {
+  Router,
+  CanActivate,
+  RouterStateSnapshot,
+  ActivatedRouteSnapshot,
+} from '@angular/router';
+import { Observable } from 'rxjs';
+
+import { AuthService } from './auth.service';
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+  constructor(private router: Router, private auth: AuthService) {}
+
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ): Observable<boolean> | Promise<boolean> | boolean {
+    if (this.auth.isAuthenticated()) {
+      return true;
+    } else {
+      this.auth.logout();
+      this.router.navigate(['/admin', 'login'], {
+        queryParams: {
+          loginAgain: true,
+        },
+      });
+    }
+  }
+}
