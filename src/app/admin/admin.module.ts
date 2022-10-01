0788f5a372261870b4179f626f458cf71be05b92
Pavel Kizhlo <pavelkizh@gmail.com>
2022-10-01 21:57:24 +0300

    feat: create auth interceptor

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -52,6 +52,6 @@ import { AuthGuard } from './shared/services/auth.guard';
     ]),
   ],
   exports: [RouterModule],
-  providers: [AuthService, AuthGuard],
+  providers: [AuthGuard],
 })
 export class AdminModule {}
